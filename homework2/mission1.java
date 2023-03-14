f24e6e41f4e09dae6d9e22ce446c807633963f9c
Annet7 <113839507+Annet7@users.noreply.github.com>
2023-03-14 18:03:09 +0700

    Update mission1.java

--- homework2/mission1.java
+++ homework2/mission1.java
@@ -3,11 +3,21 @@ package homeWork.homework2;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class mission1 {
     public static void main(String[] args) {
-    int[] myArr = {5,1,2,3,4};
+    int[] myArr = new int[10];
+    for (int i = 0; i < myArr.length; i++) {
+    myArr[i] = (int)(Math.random() * 10);
+    System.out.println(myArr);
+}
 
     String fileName = "log.txt";
     File  file = new File(fileName);
@@ -18,9 +28,11 @@ public class mission1 {
         isSorted = true;
     for (int i = 0; i < myArr.length - 1; i++) {
         if (myArr[i] > myArr[i+1]){
+            isSorted = false;
             item = myArr[i];
             myArr[i] = myArr[i+1];
             myArr[i+1] = item;
+            // Подскажите, пожалуйста, если делаю вот таким "примитивным" методом, то все работает, а если пытаюсь также, как Вы показывали на семинаре (строки 54-70), то создается пустой журнал логов.
             try{
                 String strMyArr = Arrays.toString(myArr);
                 FileWriter writer = new FileWriter(file, true);    
@@ -32,8 +44,32 @@ public class mission1 {
             System.out.println("ЧТО-ТО ПОШЛО НЕ ТАК...");
         }    
         }        
-    }
+        }
     System.out.println(Arrays.toString(myArr));
         }
-    }
+
+/**
+ * Innermission1
+ */
+// public class lg {
+//     public static void main(String[] args) throws IOException{
+//         Logger ll = Logger.getLogger(lg.class.getName());
+//         String logsPath = "log.txt";
+//         FileHandler fh = new FileHandler(logsPath, false);
+//         ll.addHandler(fh);
+//         SimpleFormatter formatter = new SimpleFormatter();
+//         fh.setFormatter(formatter);
+//         Random rand = new Random();
+//         for (int i = 0; i <5; i++) {
+//             int a = rand.nextInt(10);
+//             ll.log(Level.INFO, "element: " + a);
+//         }
+//         ll.log(Level.WARNING, "logger off");
+//         ll.info("test");
+//         ll.warning("logsPath");
+//     }
+    
 }
+}
+    
+
